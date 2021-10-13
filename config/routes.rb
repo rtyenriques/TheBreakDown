1b138ce400455a1b30f648cfaba8300a276411b0
= <rtyenriques@gmail.com>
2021-10-13 15:58:55 -0700

    added users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'welcome#home'
   get '/home', to: 'welcome#home'
+
+  get '/signup', to: 'users#new', as: 'user'
+  post '/signup', to: 'users#create'
+  get '/profile/:id', to: 'users#show', as: 'profile'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete "/logout", to: "sessions#destroy"
+
+  resources :users
 end
